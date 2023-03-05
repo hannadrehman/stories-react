f0a3a7c7cf008f2cd26c89115aae3bfb09e0e07e
hannad rehman <hanad.rehman@healthify.co>
2023-03-05 18:30:05 +0530

    builds new docs

--- docs/assets/js/bb849e8d.079efe99.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[421],{3769:function(n){n.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
--- docs/assets/js/e72cb3ed.25945a77.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[767],{5745:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
